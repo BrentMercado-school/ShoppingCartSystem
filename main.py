64484d72a6e794f028efb557306efc4eea000df5
Brent Mercado <brentallen.mercado@benilde.edu.ph>
2025-11-07 17:34:19 +0800

    Added available products

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+products = [
+    {
+        'name': 'Apple',
+        'price': 20.00,
+        'quantity': 50
+    },
+    {
+        'name': 'Banana',
+        'price': 15.00,
+        'quantity': 40
+    },
+    {
+        'name': 'Milk',
+        'price': 50.00,
+        'quantity': 30
+    },
+    {
+        'name': 'Bread',
+        'price': 35.00,
+        'quantity': 20
+    }
+]
+
 while True:
     print("==============================")
     print("\tWELCOME TO PYTHON MART")
