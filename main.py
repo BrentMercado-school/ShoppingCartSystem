f8e7468a8e21e17451ad8a77893c7a1d42b6637b
Brent Mercado <brentallen.mercado@benilde.edu.ph>
2025-11-07 17:33:52 +0800

    Added main menu

---
+++ main.py
@@ -0,0 +1,8 @@
+while True:
+    print("==============================")
+    print("\tWELCOME TO PYTHON MART")
+    print("==============================")
+    print("1. View Products\n2. Add to Cart\n3. Remove from Cart\n4. View Cart"
+          "\n5. Checkout\n6. Exit")
+    print("------------------------------")
+    choice = input("Enter your choice: ")
